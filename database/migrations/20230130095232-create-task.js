83d48e490be35e8941df800b649fb6cc9e8e7909
sidx255 <siddharth190820@gmail.com>
2023-01-31 11:57:27 +0530

    chore: add eslint migrations

--- database/migrations/20230130095232-create-task.js
+++ database/migrations/20230130095232-create-task.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Tasks', {
+    await queryInterface.createTable("Tasks", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -26,6 +26,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Tasks');
+    await queryInterface.dropTable("Tasks");
   }
 };
